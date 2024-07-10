55ddadee4902ea5a74bb556642f3c8cb72a430f2
duke <dukebs1128@gmail.com>
2024-07-10 17:03:15 +0900

    feat : card 작업 중

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,8 +5,8 @@ import styles from './Card.module.css'
 const DevCard = ({Name, Created} : {Name : string, Created : string}) => {
     return (
         <div className={styles.card}>
-            <figure>
-                <Image alt='' src={MyImg}/>
+            <figure className={styles.figure}>
+                <Image alt='' src={MyImg} layout='responsive'/>
             </figure>
             <div className={styles.info}>
                 <span>
