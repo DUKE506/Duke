05358b130c70f06dfbc471d78536b676c41871b0
duke <dukebs1128@gmail.com>
2024-07-19 15:23:42 +0900

    feat : DnD 스터디중

--- src/app/(dev)/dev/add/components/DevImg/DevImg.tsx
+++ src/app/(dev)/dev/add/components/DevImg/DevImg.tsx
@@ -1,13 +1,13 @@
+import styles from './DevImg.module.css'
 
 
 const DevImg = () =>{
     return(
-        <div className="imgarea">
-            <div>
-                <span>
-                    이미지 등록
-                </span>
-            </div>
+        <div className={styles.img_container}>
+            
+            <span>
+                이미지 등록
+            </span>
             <div>
                 
             </div>
